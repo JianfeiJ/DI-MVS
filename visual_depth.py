f5c9e70fedd675fe84bf29fe221c6163c2adbebf
JianfeiJ <98368257+JianfeiJ@users.noreply.github.com>
2023-09-18 10:08:34 +0800

    Add files via upload

---
+++ visual_depth.py
@@ -0,0 +1,57 @@
+import matplotlib as mpl
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import os
+import cv2
+from PIL import Image
+from utils import visualize_depth
+
+
+def read_pfm(filename):
+    file = open(filename, 'rb')
+    color = None
+    width = None
+    height = None
+    scale = None
+    endian = None
+
+    header = file.readline().decode('utf-8').rstrip()
+    if header == 'PF':
+        color = True
+    elif header == 'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+
+    dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode('utf-8'))
+    if dim_match:
+        width, height = map(int, dim_match.groups())
+    else:
+        raise Exception('Malformed PFM header.')
+
+    scale = float(file.readline().rstrip())
+    if scale < 0:  # little-endian
+        endian = '<'
+        scale = -scale
+    else:
+        endian = '>'  # big-endian
+
+    data = np.fromfile(file, endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+
+    data = np.reshape(data, shape)
+    data = np.flipud(data)
+    file.close()
+    return data, scale
+
+
+depth_path = ''  # single pfm path
+
+depth, scale = read_pfm(depth_path)
+
+ma = np.ma.masked_equal(depth, 0.0, copy=False)
+print('value range: ', ma.min(), ma.max())
+plt.imshow(depth, 'viridis')
+plt.show()
